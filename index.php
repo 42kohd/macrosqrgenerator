69263107e8e3e7b9b83d9744d6bf08afdf7719f6
jer <j@j3r.org>
2022-07-21 19:00:44 -0400

    working generator

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
 
+require __DIR__ . '/vendor/autoload.php';
+
+
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Writer\PngWriter;
+
+
+
+
+
+
+
+
+$qr = QrCode::create("https://code-boxx.com");
+$writer = new PngWriter();
+$result = $writer->write($qr);
+
+
+
+
+echo "<img src='{$result->getDataUri()}'/>";
 
