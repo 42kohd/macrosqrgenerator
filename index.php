34288c8a52d474a5d81f9170fe861febde29b326
jer <j@j3r.org>
2022-07-21 19:08:32 -0400

    added form to send coach name

--- index.php
+++ index.php
@@ -1,24 +1,55 @@
 <?php
-
 require __DIR__ . '/vendor/autoload.php';
-
 
 use Endroid\QrCode\QrCode;
 use Endroid\QrCode\Writer\PngWriter;
 
+$coach = $_POST['coach'];
 
 
 
-
-
-
-
-$qr = QrCode::create("https://code-boxx.com");
+$qr = QrCode::create($coach);
 $writer = new PngWriter();
 $result = $writer->write($qr);
+?>
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>MI Generator</title>
+  <meta name="description" content="A simple HTML5 Template for new projects.">
+  <meta name="author" content="SitePoint">
+
+  <meta property="og:title" content="A Basic HTML5 Template">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://www.sitepoint.com/a-basic-html5-template/">
+  <meta property="og:description" content="A simple HTML5 Template for new projects.">
+  <meta property="og:image" content="image.png">
+
+  <link rel="icon" href="/favicon.ico">
+  <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+  <!-- your content here... -->
+
+<form method="post">
+<input type="text" name="text">
+<input type="submit">
+</form>
 
 
-
-
+<?php
 echo "<img src='{$result->getDataUri()}'/>";
+?>
+
+</body>
+</html>
 
